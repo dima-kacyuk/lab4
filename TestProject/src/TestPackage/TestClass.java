a9f5deb1376a34644987958de8d77fdfc22058a1
dima-kacyuk <dima-kacyuk>
2018-09-08 22:30:29 +0300

    Test

---
+++ TestProject/src/TestPackage/TestClass.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Каюцк Д.Г.");
+		System.out.print("\nДата рождения: 20.01.1999");
+		System.out.print("\nГруппа: \tAT - 161");
+		System.out.println("\nИзучает Ja\bVa");
+	}
+
+}
